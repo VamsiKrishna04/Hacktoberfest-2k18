aead4c0d73c1a2885aeead4cf16c8632c92bd8ff
B.Vamsi Krishna <bommisettyvamsikrishna04@gmail.com>
2018-10-01 23:50:37 +0530

    Create Concatinate_Two_Inputs.py
    Added a Python 3 program that takes 2 Inputs from a user and gives Concatenated output.

---
+++ Concatinate_Two_Inputs.py
@@ -0,0 +1,5 @@
+# To concatenate two inputs in python 3 taken by user
+input1 = input("Enter Input1")
+input2 = input("Enter Input2")
+concatenated_output = input1+input2
+print("Concatenation Of Two Inputs: " + concatenated_output)
